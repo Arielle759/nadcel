1892445fae028a21e1324bd30d5211b5f8c82755
Arielle <ganguiaarielleschekina@gmail.com>
2026-05-29 01:45:27 +0000

    Semaine 1: système de rôles et inscription personnalisée

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,15 +33,22 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+            'role' => ['required', 'string', 'in:client,manager'],   
+            ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => $request->role,
         ]);
-
+            if ($request->role === 'manager') {
+            $user->assignRole('manager');
+        } else {
+            $user->assignRole('client');
+        }
         event(new Registered($user));
 
         Auth::login($user);
